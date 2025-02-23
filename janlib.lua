6de9ebe180c8d993abe14b85149e71a16a5605e1
Zzryxx <limeroblox2019@gmail.com>
2025-02-23 03:42:28 -0500

    janlib.lua

--- janlib.lua
+++ janlib.lua
@@ -2452,7 +2452,7 @@ function library:Init()
     self.base = library:Create("ScreenGui", {IgnoreGuiInset = true, ZIndexBehavior = Enum.ZIndexBehavior.Global})
     if runService:IsStudio() then
         self.base.Parent = script.Parent.Parent
-    elseif syn then
+        self.base.Parent = gethui()
         pcall(function() self.base.RobloxLocked = true end)
         self.base.Parent = game:GetService"CoreGui"
     end
